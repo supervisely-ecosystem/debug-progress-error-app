29ade27210c117f4ae39d2abfc83faf44c8c1623
Maxim Kolomeychenko <maxim.kolomeychenko@gmail.com>
2020-09-03 13:15:36 +0300

    try simple version (aka python scripts)

--- src/main_dpe.py
+++ src/main_dpe.py
@@ -1,13 +1,13 @@
 import time
 import supervisely_lib as sly
 
-my_app = sly.AppService()
+# my_app = sly.AppService()
 
 
-@my_app.callback("run")
+# @my_app.callback("run")
 @sly.timeit
-def run(api: sly.Api, task_id, context, state, app_logger):
-    progress = sly.Progress("Processing", 10, app_logger)
+def run():
+    progress = sly.Progress("Processing", 10)
     for i in range(10):
         time.sleep(1)
         progress.iter_done_report()
@@ -15,10 +15,12 @@ def run(api: sly.Api, task_id, context, state, app_logger):
 
 
 def main():
-    initial_events = [{"state": None, "context": None, "command": "run"}]
+    # initial_events = [{"state": None, "context": None, "command": "run"},
+    #                   {"state": None, "context": None, "command": "stop"}]
 
     # Run application service
-    my_app.run(data={}, state={}, initial_events=initial_events)
+    # my_app.run(data={}, state={}, initial_events=initial_events)
+    run()
 
 
 if __name__ == "__main__":
