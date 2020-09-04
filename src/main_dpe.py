e1bfc62697ed822f130e225376592b62b4e96c2d
Maxim Kolomeychenko <maxim.kolomeychenko@gmail.com>
2020-09-04 09:29:00 +0300

    clean comments + 100 seconds for debug

--- src/main_dpe.py
+++ src/main_dpe.py
@@ -1,26 +1,14 @@
 import time
 import supervisely_lib as sly
 
-# my_app = sly.AppService()
 
-
-# @my_app.callback("run")
 @sly.timeit
-def run():
-    progress = sly.Progress("Processing", 10)
-    for i in range(10):
+def main():
+    progress = sly.Progress("Processing", 100)
+    for i in range(100):
         time.sleep(1)
         progress.iter_done_report()
     raise RuntimeError("Debug error")
-
-
-def main():
-    # initial_events = [{"state": None, "context": None, "command": "run"},
-    #                   {"state": None, "context": None, "command": "stop"}]
-
-    # Run application service
-    # my_app.run(data={}, state={}, initial_events=initial_events)
-    run()
 
 
 if __name__ == "__main__":
