a06735486d689f555715651814b43bc2c9eb9ade
Max Kolomeychenko <maxim.kolomeychenko@gmail.com>
2023-01-13 15:45:06 +0000

    check /mount_folder

--- src/main_dpe.py
+++ src/main_dpe.py
@@ -4,6 +4,11 @@ import supervisely_lib as sly
 
 @sly.timeit
 def main():
+    
+    check_dir = "/mount_folder"
+    if sly.fs.dir_exists(check_dir):
+        sly.fs.log_tree(check_dir, sly.logger)
+    
     progress = sly.Progress("Processing", 20)
     for i in range(20):
         time.sleep(1)
