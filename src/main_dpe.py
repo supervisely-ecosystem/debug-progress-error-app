1d2f54dd4d9a434ee386c4c638444d6634eb1602
Theo <theo.lisin@gmail.com>
2023-08-29 23:45:44 +0300

    produce not found docker error

--- src/main_dpe.py
+++ src/main_dpe.py
@@ -5,11 +5,11 @@ import supervisely_lib as sly
 @sly.timeit
 def main():
     
-    check_dir = "/mount_folder"
-    if sly.fs.dir_exists(check_dir):
-        files = sly.fs.list_dir_recursively(check_dir)
-        for path in files:
-            sly.logger.info(f"File: {path}")
+    # check_dir = "/mount_folder"
+    # if sly.fs.dir_exists(check_dir):
+    #     files = sly.fs.list_dir_recursively(check_dir)
+    #     for path in files:
+    #         sly.logger.info(f"File: {path}")
         
     
     progress = sly.Progress("Processing", 20)
