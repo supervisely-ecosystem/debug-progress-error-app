415537ea73b7bcf9357a965615d896a51a3124af
TheoLisin <theo.lisin@gmail.com>
2023-09-15 16:09:32 +0000

    increase cycle and rm raises

--- src/main_dpe.py
+++ src/main_dpe.py
@@ -5,18 +5,20 @@ import supervisely_lib as sly
 @sly.timeit
 def main():
     
-    check_dir = "/mount_folder"
-    if sly.fs.dir_exists(check_dir):
-        files = sly.fs.list_dir_recursively(check_dir)
-        for path in files:
-            sly.logger.info(f"File: {path}")
+    # check_dir = "/mount_folder"
+    # if sly.fs.dir_exists(check_dir):
+    #     files = sly.fs.list_dir_recursively(check_dir)
+    #     for path in files:
+    #         sly.logger.info(f"File: {path}")
         
     
-    progress = sly.Progress("Processing", 20)
-    for i in range(20):
+    progress = sly.Progress("Processing", 1000)
+    for i in range(1000):
         time.sleep(1)
         progress.iter_done_report()
-    raise RuntimeError("Debug error")
+        if i % 20 == 0:
+            sly.logger.info("I'M STILL ALIVE")
+    # raise RuntimeError("Debug error")
 
 
 if __name__ == "__main__":
